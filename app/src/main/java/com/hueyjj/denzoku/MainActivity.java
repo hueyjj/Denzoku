857f235971f417e56c7dedb18afad877a93c70a8
hueyjj <jasperjeng@gmail.com>
2018-04-15 23:25:14 -0700

    Code to test network request

--- app/src/main/java/com/hueyjj/denzoku/MainActivity.java
+++ app/src/main/java/com/hueyjj/denzoku/MainActivity.java
@@ -12,6 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.hueyjj.denzoku.network.MalNetworkRequest;
+import com.hueyjj.denzoku.network.NyaaNetworkRequest;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +44,24 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NyaaNetworkRequest req = new NyaaNetworkRequest(getApplicationContext());
+                req.setQuery("Boku no hero academia");
+                req.build();
+                req.getNyaaList();
+            }
+        });
+        //btn.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        MalNetworkRequest req = new MalNetworkRequest(getApplicationContext(), "hueyjj");
+        //        req.getAnimeList();
+        //    }
+        //});
     }
 
     @Override
